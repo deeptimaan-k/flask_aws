7aa7b6e36672eedf92d866bae08308590a3550bf
Radiant <76729397+deeptimaan-k@users.noreply.github.com>
2025-01-28 23:16:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 db_config = {
     'host': 'database-1.cz6sa4gmko3k.eu-north-1.rds.amazonaws.com',
-    'database': 'database-1',  
+    'database': 'flaskapp',  
     'user': 'admin',         
     'password': 'radiant4452'  
 }
